d98f9f6f374627b260df88c05d3ffc6e7b232ecf
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-11-03 13:48:02 +0100

    ad js

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,37 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn1: document.querySelector('[data-modal-open-first]'),
+      closeModalBtn1: document.querySelector('[data-modal-close-first]'),
+      modal: document.querySelector('[data-modal-first]'),
+    };
+  
+    refs.openModalBtn1.addEventListener('click', toggleModal);
+    refs.closeModalBtn1.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openModalBtn2: document.querySelector('[data-modal-open-second]'),
+      closeModalBtn2: document.querySelector('[data-modal-close-second]'),
+      modal: document.querySelector('[data-modal-second]'),
+    };
+  
+    refs.openModalBtn2.addEventListener('click', toggleModal);
+    refs.closeModalBtn2.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+
+
+  
